53dc5647583cf91aad6e4eb34c438900bf78fde2
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-15 19:07:51 -0300

    feat: estilization of StatusBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View, Text, StatusBar} from 'react-native';
+import {View, Text, StatusBar} from 'react-native'
 
 const App:React.FC = () =>{
   return(
     <>
-      <StatusBar barStyle="dark-content" backgroundColor="#fff"/>
-      <View> 
-        <Text> Elixir 3.4 </Text>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38"  />
+      <View style={{ flex: 1, backgroundColor:'#312e38' }}>
+        <Text> GoBarber</Text>
       </View>
     </>
   )
